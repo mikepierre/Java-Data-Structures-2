a7a7de304068b9da800f26d6ed85168899139dbc
mikepierre <mikeypierre84@yahoo.com>
2017-07-17 15:51:13 -0400

    Create subStringComparison.java
    Given a string, 8, and an integer, k, complete the function so that it finds the lexicographically smallest and largest substrings of length k.

---
+++ Strings/subStringComparison.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class subStringComparison {
+
+	public static void main(String[] args) {
+		
+		String s = "welcometojava";
+		int k = 3;
+		
+		System.out.println(getSmallestAndLargest(s, k));
+		
+	}
+	
+    public static String getSmallestAndLargest(String s, int k) {
+        String smallest = "";
+        String largest = "";
+        
+        List<String> sub = new ArrayList<String>();  
+        for(int i=0; i<=(s.length()-k); i++ ){
+            sub.add(s.substring(i, k+i));
+        }
+
+        Collections.sort(sub);
+        
+        smallest = sub.get(0);
+        largest = sub.get(sub.size()-1);
+        return smallest + "\n" + largest;
+    }
+
+    
+
+}
