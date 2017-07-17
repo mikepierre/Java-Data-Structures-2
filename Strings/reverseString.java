d31cf1e075e99e905578ec784f5f0629cd23aab4
mikepierre <mikeypierre84@yahoo.com>
2017-07-17 13:56:15 -0400

    Reverse String
    Reverse phrase in a string so from The Sky is Blue to Blue is the Sky.

---
+++ Strings/reverseString.java
@@ -0,0 +1,21 @@
+public class reverseString {
+
+	public static void main(String[] args) {
+		
+		String s = "The Sky is Blue A B C";
+		
+		System.out.println(reverse(s));
+	
+	}
+	
+	public static String reverse(String str){
+		String[] oldString = str.split(" ");
+		String newString ="";
+		
+		for(int i = oldString.length - 1; i >= 0; --i){
+			newString += " "+oldString[i];
+		}
+		return newString.substring(1);
+	}
+	
+}
