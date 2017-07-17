05e008784d2dc28a7d18022b284e5dc582743c82
mikepierre <mikeypierre84@yahoo.com>
2017-07-17 15:08:30 -0400

    added substring excersise
    Given a string, s , and two indices,  and , print a substring consisting of all characters in the inclusive range from start to end -1 . You'll find the String class' substring method helpful in completing this challenge.

---
+++ Strings/subString.java
@@ -0,0 +1,11 @@
+public class subString {
+
+	public static void main(String[] args) {
+		String s = "Helloworld";
+		int start = 3;
+		int end = 7;
+		System.out.println(s.substring(start, end));
+		
+	}
+	
+}
