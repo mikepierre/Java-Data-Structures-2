88e124083a75f4585e614fb5c28be22d9c371c48
mikepierre <mikeypierre84@yahoo.com>
2017-07-17 22:04:22 -0400

    Create stringTokens.java
    Given a string, , matching the regular expression [A-Za-z !,?._'@]+, split the string into tokens. We define a token to be one or more consecutive English alphabetic letters. Then, print the number of tokens, followed by each token on a new line.

---
+++ Strings/stringTokens.java
@@ -0,0 +1,14 @@
+public class regex {
+
+	public static void main(String[] args) {
+
+		String s = "He is a very very good boy, isn't he?";
+
+		String[] parts = s.split("[ !,?._'@]+");
+		System.out.println(parts.length);
+		for(int i =0; i < parts.length; i++){
+			System.out.println(parts[i]);
+		}
+	}
+
+}
