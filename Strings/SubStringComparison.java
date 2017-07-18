f2d062ace15d677ed8a42783256fd22757ab4839
mikepierre <mikeypierre84@yahoo.com>
2017-07-18 11:06:34 -0400

    Update and rename subStringComparison.java to SubStringComparison.java

--- Strings/SubStringComparison.java
+++ Strings/SubStringComparison.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public class subStringComparison {
+public class SubStringComparison {
 
 	public static void main(String[] args) {
 		
